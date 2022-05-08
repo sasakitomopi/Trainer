19bb3f6f81cc881a9b1a7298ac6b0a17e43c7b0a
Tomohiro Sasaki <takatobi0326@icloud.com>
2022-05-08 11:51:29 +0900

    Initial Commit

---
+++ Trainer/TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainerApp.swift
+//  Trainer
+//
+//  Created by user on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
